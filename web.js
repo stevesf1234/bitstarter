814be883b485685975ee39fd1dc29bb7868767b1
stevesf1234 <stevesf1234@gmail.com>
2013-08-28 23:14:29 +0000

    Added api-enabled to html tag

--- web.js
+++ web.js
@@ -3,7 +3,7 @@ var express = require('express');
 var app = express.createServer(express.logger());
 
 var fs = require('fs');
-var data = fs.readFileSync('index.html');
+var data = fs.readFileSync('sender/index.html');
 
 app.get('/', function(request, response) {
   response.send(data.toString());
