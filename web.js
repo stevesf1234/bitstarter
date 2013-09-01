3e144b4d5807a44bfae0ccc7076738f5b2b05acb
stevesf1234 <stevesf1234@gmail.com>
2013-09-01 17:09:15 +0000

    Updated web.js

--- web.js
+++ web.js
@@ -2,16 +2,22 @@ var express = require('express');
 var fs = require('fs');
 var path = require('path');
 var http = require('http');
+var ejs = require('ejs');
 
 var app = express();
 app.set('port', process.env.PORT || 8080);
+app.set('view engine', 'ejs');
 app.use(express.static(__dirname + '/public'));
 
-app.get('/', function(request, response) {
-  var data = fs.readFileSync('index.html').toString();
-  response.send(data);
+app.get('/', function(req, res) {
+  res.send(fs.readFileSync('./index.html').toString());
+});
+
+app.get('/sender', function(req, res) {
+  res.send(fs.readFileSync('./sender/index.html').toString());
 });
 
 http.createServer(app).listen(app.get('port'), function() {
   console.log("Listening on " + app.get('port'));
 });
+
